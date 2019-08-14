ce1d25d44657415f5b230191e215be40f54d8339
George <klepach.george@gmail.com>
2019-08-14 12:26:37 +0300

    gcp hw2 add y to apt install

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -3,6 +3,6 @@ sudo rm /etc/apt/sources.list.d/mongodb*.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.0.list'
 sudo apt update
-sudo apt install mongodb-org
+sudo apt install y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
